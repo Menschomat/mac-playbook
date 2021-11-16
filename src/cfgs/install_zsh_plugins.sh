978055e1d438b001d7d7e288fb44036dfe7d32ff
Falco Weihrauch <falco@weihrauch.me>
2021-11-16 22:52:04 +0100

    update

---
+++ src/cfgs/install_zsh_plugins.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
